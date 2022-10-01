8c2d43c800b4d74a6fe757a9f4194598e4ea34ed
thomasmunguya <thomasmunguya@gmail.com>
2022-10-01 07:54:53 +0200

    Aligned UI components on main screen.

--- src/com/theschool/clock/Clock.java
+++ src/com/theschool/clock/Clock.java
@@ -1,10 +1,12 @@
 package com.theschool.clock;
 
 
+import com.theschool.clock.gui.StopwatchGUI;
 import javax.swing.*;
+import java.awt.*;
 
 /**
- * The {@code Clock} class represents the entry point for the application.
+ * The {@code Clock} class represents the entry point for the clock application.
  */
 public class Clock {
     /**
@@ -27,7 +29,58 @@ public class Clock {
      */
     private JButton exitButton;
 
-    public static void main(String[] args) {
-    }
+    /**
+     * Defines the graphical user interface for the stopwatch.
+     */
+    private static StopwatchGUI stopwatchGUI;
 
+    /**
+     * Defines the JFrame for the stopwatch.
+     */
+    private static JFrame stopwatchJFrame;
+
+    /**
+     * Defines the dimensions of the screen.
+     */
+    private static final Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+
+    /**
+     * Defines the main JFrame.
+     */
+    private static final JFrame mainJFrame = new JFrame();
+
+    /**
+     * Defines an instance of this class.
+     */
+    private static final Clock clock = new Clock();
+
+    public static void main(String[] args) {
+
+        FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER);
+        flowLayout.setHgap(20);
+        JPanel startStopButtonsPanel = new JPanel(flowLayout);
+        startStopButtonsPanel.add(clock.timerButton);
+        startStopButtonsPanel.add(clock.stopwatchButton);
+
+        clock.mainJPanel.add(startStopButtonsPanel);
+
+        SpringLayout springLayout = new SpringLayout();
+        springLayout.putConstraint(SpringLayout.WEST, startStopButtonsPanel, 5, SpringLayout.WEST, clock.mainJPanel);
+        springLayout.putConstraint(SpringLayout.NORTH, startStopButtonsPanel, 10, SpringLayout.NORTH, clock.mainJPanel);
+        springLayout.putConstraint(SpringLayout.NORTH, clock.exitButton, 10, SpringLayout.SOUTH, startStopButtonsPanel);
+
+        springLayout.putConstraint(SpringLayout.WEST, clock.exitButton, 80, SpringLayout.WEST, clock.mainJPanel);
+
+        clock.mainJPanel.setLayout(springLayout);
+
+        int x = (int) ((dimension.getWidth() - mainJFrame.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - mainJFrame.getHeight()) / 2);
+
+        mainJFrame.setLocation(x, y);
+        mainJFrame.setContentPane(clock.mainJPanel);
+        mainJFrame.setTitle("Clock");
+        mainJFrame.setSize(240, 135);
+        mainJFrame.setResizable(false);
+        mainJFrame.setVisible(true);
+    }
 }
