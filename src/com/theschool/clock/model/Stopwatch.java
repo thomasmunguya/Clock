9b90861dc763f03acabe2819732c972a0d89c8f0
thomasmunguya <thomasmunguya@gmail.com>
2022-10-01 09:35:01 +0200

    Documented exceptions for Stopwatch class.

--- src/com/theschool/clock/model/Stopwatch.java
+++ src/com/theschool/clock/model/Stopwatch.java
@@ -9,6 +9,8 @@ import javax.swing.text.JTextComponent;
  *
  * @see Time
  * @see Timeable
+ *
+ * @author Thomas Munguya
  */
 public class Stopwatch extends Timeable implements Runnable {
 
@@ -22,7 +24,8 @@ public class Stopwatch extends Timeable implements Runnable {
     /**
      * Constructs a {@code Stopwatch} with the provided arguments.
      * @param running the running state of this stopwatch.
-     * @param component the component to write the time to.                
+     * @param component the component to write the time to.
+     * @throws IllegalArgumentException if {@code component} is {@code null}.
      */
     public Stopwatch(boolean running, JTextComponent component) {
         super(running, component);
